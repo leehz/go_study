fdf67af2b33f6c0c5e6c529efac901a6a482f0c4
leehz <hz@huaixiaoz.com>
2015-03-30 09:56:32 +0800

    add exported-names.go functions.go

---
+++ package/functions.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func add1(x, y int) int {
+	return x * y
+}
+
+func main() {
+	fmt.Println(add(42, 14))
+	fmt.Println(add1(5, 11))
+}
